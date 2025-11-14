33fd02239c5dc186c73f0c74092331dfc67f1f74
TruongNguyenDinh <truongnd1124@gmail.com>
2025-11-14 09:03:22 +0700

    Add more comments

--- Python/action/src/action_nodes/action_nodes/fibonacci_action_client.py
+++ Python/action/src/action_nodes/action_nodes/fibonacci_action_client.py
@@ -13,17 +13,22 @@ class FiboActionClient(Node):
             'fibonacci' #Tên action
         )
     def send_goal(self,order):
+            # Khởi tạo goal
             goal_msg = Fibonacci.Goal()
+            # Lấy mục tiêu
             goal_msg.order = order
+            # Đợi server, nếu server không phản hồi thì sẽ chặn ở đây
             self._action_client.wait_for_server()
-
+            # Gửi goal đến server và feedback
             self._send_goal_future = self._action_client.send_goal_async(
                 goal_msg,
-                feedback_callback = self.feedback_callback
+                feedback_callback = self.feedback_callback #Gửi feedback và nhận
             )
+            #Gọi callback kết quả
             self._send_goal_future.add_done_callback(self.goal_response_callback)
         
     def goal_response_callback(self,future):
+        #Trạng thái của phản hồi sau khi kết nối tới server từ chối | chấp nhận
         goal_handle = future.result()
         if not goal_handle.accepted:
             self.get_logger().info('Goal rejected :(')
@@ -34,11 +39,14 @@ class FiboActionClient(Node):
         self._get_result_future.add_done_callback(self.get_result_callback)
 
     def get_result_callback(self, future):
+        #Nhận kết quả cuối cùng khi server hoàn tất
         result = future.result().result
         self.get_logger().info(f'result: {result.sequence}')
-        rclpy.shutdown()  
+        rclpy.shutdown() 
+     
     def feedback_callback(self, feedback_msg):
         feedback = feedback_msg.feedback
+        #Debug feedback
         self.get_logger().info(f'Received: {feedback.partial_sequence}')
         
 def main(args=None):
